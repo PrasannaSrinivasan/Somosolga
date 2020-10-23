9a5d2221c78aa6c0c4974058f4e89720523a31c1
PrasannaSrinivasanV <68372583+PrasannaSrinivasanV@users.noreply.github.com>
2020-10-23 16:31:14 +0530

    Add files via upload

---
+++ src/containers/Layout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Layout extends Component{
+    render(){
+        return (
+            <Header/>
+            //Main
+            //Footer
+        );
+    }
+}
+
+export default Layout;
